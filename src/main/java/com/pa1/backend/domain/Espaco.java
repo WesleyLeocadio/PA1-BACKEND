6a1db3400902d9f4440b321e39cf040310879280
weslley leocadio <silvawesley374@gmail.com>
2019-09-05 22:11:38 -0300

    Testando a primeira classe de dominio - Espaco

---
+++ src/main/java/com/pa1/backend/domain/Espaco.java
@@ -0,0 +1,113 @@
+package com.pa1.backend.domain;
+
+import java.io.Serializable;
+
+//implementando a interface serializable( os objetos sao convertidos em uma sequencia de bytes) pra ser gravados em arquivos, trafegar em rede...
+public class Espaco implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private int idEspaco;
+	private String espacoNome;
+	private String espacoDescricao;
+    private String espacoLocalizacao;
+    private boolean espacoEspecial;
+    private String espacoResponsavel;
+    private boolean espacoDesabilitado;
+    
+    public Espaco() {}
+
+	public Espaco(int idEspaco, String espacoNome, String espacoDescricao, String espacoLocalizacao,
+			boolean espacoEspecial, String espacoResponsavel, boolean espacoDesabilitado) {
+		super();
+		this.idEspaco = idEspaco;
+		this.espacoNome = espacoNome;
+		this.espacoDescricao = espacoDescricao;
+		this.espacoLocalizacao = espacoLocalizacao;
+		this.espacoEspecial = espacoEspecial;
+		this.espacoResponsavel = espacoResponsavel;
+		this.espacoDesabilitado = espacoDesabilitado;
+	}
+
+	public int getIdEspaco() {
+		return idEspaco;
+	}
+
+	public void setIdEspaco(int idEspaco) {
+		this.idEspaco = idEspaco;
+	}
+
+	public String getEspacoNome() {
+		return espacoNome;
+	}
+
+	public void setEspacoNome(String espacoNome) {
+		this.espacoNome = espacoNome;
+	}
+
+	public String getEspacoDescricao() {
+		return espacoDescricao;
+	}
+
+	public void setEspacoDescricao(String espacoDescricao) {
+		this.espacoDescricao = espacoDescricao;
+	}
+
+	public String getEspacoLocalizacao() {
+		return espacoLocalizacao;
+	}
+
+	public void setEspacoLocalizacao(String espacoLocalizacao) {
+		this.espacoLocalizacao = espacoLocalizacao;
+	}
+
+	public boolean isEspacoEsopecial() {
+		return espacoEspecial;
+	}
+
+	public void setEspacoEsopecial(boolean espacoEsopecial) {
+		this.espacoEspecial = espacoEsopecial;
+	}
+
+	public String getEspacoResponsavel() {
+		return espacoResponsavel;
+	}
+
+	public void setEspacoResponsavel(String espacoResponsavel) {
+		this.espacoResponsavel = espacoResponsavel;
+	}
+
+	public boolean isEspacoDesabilitado() {
+		return espacoDesabilitado;
+	}
+
+	public void setEspacoDesabilitado(boolean espacoDesabilitado) {
+		this.espacoDesabilitado = espacoDesabilitado;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idEspaco;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Espaco other = (Espaco) obj;
+		if (idEspaco != other.idEspaco)
+			return false;
+		return true;
+	}
+    
+    //Para que os objetos seram comparados pelos seus conteudos (nao pelos ponteiros de memoria) tem que implementar os metodos hash e equals
+	
+    
+
+}
--- src/main/java/com/pa1/backend/resources/EspacoResouce.java
+++ src/main/java/com/pa1/backend/resources/EspacoResouce.java
@@ -1,16 +1,26 @@
 package com.pa1.backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pa1.backend.domain.Espaco;
 //classe vai ser um controlador REST
 @RestController
 @RequestMapping(value = "/espacos") //vai responder por este endPoint
 public class EspacoResouce {
 	//pra ser uma função REST é preciso associar com algum dos verbos do http
 	@RequestMapping(method = RequestMethod.GET)//como tô  obtendo dados uso o metodo get
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Espaco> listar() {
+		Espaco esp1= new Espaco(1,"lab informatica","sala com computadores","predio de informatica",false,"Laura",false);
+		Espaco esp2= new Espaco(2,"Sala teorica","sala comum","predio de informatica",false,"Taniro",false);
+		List<Espaco> list=new ArrayList<Espaco>();
+		list.add(esp1);
+		list.add(esp2);
+		return list;
 	}
 
 }
