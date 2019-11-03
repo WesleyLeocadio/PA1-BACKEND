71b555ff6daad0fe054edf403ef4372fb895cc32
biachacon <anabheatrizchacon@outlook.com>
2019-11-03 15:37:25 -0300

    swagger na home

--- src/main/java/com/pa1/backend/StartApplication.java
+++ src/main/java/com/pa1/backend/StartApplication.java
@@ -1,26 +1,13 @@
 package com.pa1.backend;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.pa1.backend.domain.Espaco;
-import com.pa1.backend.domain.Reserva;
-import com.pa1.backend.domain.Usuario;
-import com.pa1.backend.repositories.EspacoRepository;
-import com.pa1.backend.repositories.ReservaRepository;
-import com.pa1.backend.repositories.UsuarioRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/")
 @SpringBootApplication
 public class StartApplication implements CommandLineRunner {
 
@@ -34,7 +21,7 @@ public class StartApplication implements CommandLineRunner {
 
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping("/")
 	public String api(){
 		return "swagger-ui.html";
 	}
